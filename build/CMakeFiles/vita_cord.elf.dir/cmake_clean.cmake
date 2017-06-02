703f42415e039a58d11ac4f9a0e5177f384b467f
devingDev <bizzy933@gmail.com>
2017-06-02 18:40:28 +0200

    using build folder , some small stuff

---
+++ build/CMakeFiles/vita_cord.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vita_cord.elf.dir/src/main.cpp.obj"
+  "CMakeFiles/vita_cord.elf.dir/src/VitaPad.cpp.obj"
+  "CMakeFiles/vita_cord.elf.dir/src/VitaIME.cpp.obj"
+  "vita_cord.elf.pdb"
+  "vita_cord.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vita_cord.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
