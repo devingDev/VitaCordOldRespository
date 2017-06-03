dc8f0814953825f1f8b31a21147d71b6c94fef78
devingDev <bizzy933@gmail.com>
2017-06-03 13:06:27 +0200

    testing gitignore

--- build/CMakeFiles/vita_cord.vpk.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vita_cord.vpk"
-  "vita_cord.vpk_param.sfo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vita_cord.vpk.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
